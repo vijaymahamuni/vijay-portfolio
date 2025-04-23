ad207aafd7f54d0a4e50902c42796274c142a58e
Vijay <vijaysethu0101@gmail.com>
2025-04-23 13:35:40 +0530

    footing page unused var fixed

--- src/components/FootingPage.js
+++ src/components/FootingPage.js
@@ -1,6 +1,4 @@
-import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import { Email } from "@mui/icons-material";
 const FootingPage = () => {
   return (
